15b7abfc3e92b51e0643e174d9de18a3f4e60b43
francesca silva <frannysilva@hotmail.com>
2025-05-21 05:48:40 -0500

    se cambio https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-         if (env('APP_ENV') !== 'local') {
-        URL::forceScheme('https');
-    }
+        //
     }
 
     /**
@@ -22,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
